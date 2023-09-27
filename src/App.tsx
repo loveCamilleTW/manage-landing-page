0b89eccacc5c2e84f7db319bc007cfccbbe515df
loveCamilleTW <yoyopetergod@gmail.com>
2023-09-27 14:05:07 +0800

    Feature: add basic HTML and contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import './App.scss'
+import { Header } from './component/Header/Header';
+import { Content } from './component/Content/Content';
+
 function App() {
   return (
     <>
+      <Header />
+      <Content />
     </>
   )
 }
---
+++ src/component/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    const options = ["Home", "Pricing", "Products", "About Us",
+        "Careers", "Community", "Privacy", "Policy"]
+
+    return (
+        <>
+            Simplify how your team works today.
+            <input placeholder=" Updates in your inbox…"></input>
+            <button>Go</button>
+            <div>Copyright 2020. All Rights Reserved</div>
+        </>
+    )
+}
